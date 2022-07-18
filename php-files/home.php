086e76e172431e8fa1611e91a1baf6669bd04b64
Deepti-tech <80256433+Deepti-tech@users.noreply.github.com>
2022-07-18 13:59:32 +0530

    Create home.php

---
+++ php-files/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+        <link rel="stylesheet" href="style/header.css">
+        <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+    </head>
+    <body>
+        <header>
+            <a href="home.php"><img src="images/travel_logo.png" alt="Travel" class="logo"></a>
+            <nav>
+                <a href="home.php">Home</a>
+                <a href="about.php">About</a>
+                <a href="package.php">Package</a>
+                <a href="book.php">Book</a>
+            </nav>
+            <div id="menu-btn" class="fas fa-times"></div>
+        </header>
+    </body>
+    <script src="script.js"></script>
+    <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+</html>
